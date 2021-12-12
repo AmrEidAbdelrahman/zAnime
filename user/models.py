2df696a948310d3133d9f18a2b24909d472a77e3
AmrEidAbdelrahman <amreid248@gmail.com>
2021-12-12 23:48:22 +0200

    login page desgin with the static files not found 404

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from  django.contrib.auth.models import User
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	pic = models.ImageField()
+	pic = models.ImageField(default='default.png', upload_to='profile_images')
 
 	def __str__(self):
-		return f'{self.user.usename} Profile'
+		return f'{self.user.username} Profile'
--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def profile(request):
+	return render(request, 'user/profile.html')
+
+
+def register(request):
+	return render(request, 'user/register.html')
