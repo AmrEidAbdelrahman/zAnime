63736245c35afdbd0c65094fe59332d102e8224c
AmrEidAbdelrahman <amreid248@gmail.com>
2021-12-14 21:33:57 +0200

    add manga to database and update Manga fields

--- anime/models.py
+++ anime/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+import time
 # Create your models here.
+
+'''
+TO ADD THE DATA FROM CSV FILE
+for i in range(2,99):
+    ...:     m = list(df.iloc[i].values)[1:]
+    ...:     manga = Manga(name=m[0], description=m[1], rate=m[2], number_of_members=int(m[3].replace(",","")))
+    ...:     manga.save()
+
+
+'''
+
 
 class Manga(models.Model):
 	## ---- TBC ---- ##
@@ -10,7 +22,9 @@ class Manga(models.Model):
 
 	name = models.CharField(max_length=200)
 	description = models.TextField()
-	pub_date = models.DateTimeField()
+	## ---- TBC ---- ##
+	## pub_date = models.DateField()
+	## ------------- ##
 	upload_date = models.DateTimeField(auto_now_add=True)
 	## ---- TBC ---- ##
 	kind = models.CharField(max_length=10, choices=manga_kinds, default='Advanture')
@@ -19,9 +33,9 @@ class Manga(models.Model):
 	## ---- TBC ---- ##
 	## ADD THE MANGA PROFILE IMAGE
 	## ------------- ##
-
-	rate = models.IntegerField(default=3)
-	number_of_chapters = models.IntegerField()
+	number_of_members = models.IntegerField(default=0)
+	rate = models.FloatField(default=3)
+	number_of_chapters = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.name
--- anime/views.py
+++ anime/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from .models import Manga
 # Create your views here.
 
 def index(request):
-	return render(request,'anime/index.html', {'tab':'home'})
+	manga = Manga.objects.order_by('-rate').all()
+	manga = manga[0:9]
+	context = {
+		'manga':manga,
+		'tab':'home',
+		}
+	return render(request,'anime/index.html', context)
+
+'''
+class MangaListView(ListView):
+	model = Manga
+	template_name = 'anime/index.html'
+	context_object_name = 'manga'
+	paginate_by = 9
+
+'''
