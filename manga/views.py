49c39310b85e731fadfe1897aa32398b7204a665
amr <amreid248@gmail.com>
2022-05-25 22:41:17 +0200

    manga details page

--- manga/views.py
+++ manga/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
+
+from manga.models import Manga
 
 
 class MangaView(ModelViewSet):
+    queryset = Manga.objects.all()
+    pagination_class = LimitOffsetPagination
+    pagination_by = 15
 
     def list(self, request, *args, **kwargs):
-        return render(request, 'manga/index.html')
+        qs = self.get_queryset()
+        manga_list = Paginator(qs, self.pagination_by)
+        page = request.GET.get('page', 1)
+        manga_list = manga_list.get_page(page)
+        return render(request, 'manga/index.html', {
+            'manga_list': manga_list
+        })
 
     def retrieve(self, request, *args, **kwargs):
-        return render(request, 'manga/details.html')
+        manga_pk = kwargs.get('pk')
+        manga = get_object_or_404(Manga, pk=manga_pk)
+        return render(request, 'manga/details.html', {
+            'manga': manga
+        })
